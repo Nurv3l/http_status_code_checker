803be7a2c2b148f484f19e3b5a9fcfc112b7574b
Nurulhüda Bursalı <nbursali@Nurvels-MacBook.local>
2024-01-10 10:35:11 +0300

    Updated

--- status_code_checker.py
+++ status_code_checker.py
@@ -24,40 +24,19 @@ try:
         answer = input("Save results to file? (Y/N)" + "\n")
         for x in read:
             try:
-                response = requests.get(x, headers=header).status_code
+                response = requests.get(x, timeout=1).status_code
                 if answer == "Y" or answer == "y":
-                    if response == 403:
-                        f = open("403.txt", "a")
-                        f.write(x + "\n")
-                        f.close()
-                    elif response == 503:
-                        f = open("503.txt", "a")
-                        f.write(x + "\n")
-                        f.close()
-                    elif response == 200:
-                        f = open("200.txt", "a")
-                        f.write(x + "\n")
-                        f.close()
-                    elif response == 400:
-                        f = open("400.txt", "a")
-                        f.write(x + "\n")
-                        f.close()
-                    elif response == 404:
-                        f = open("404.txt", "a")
-                        f.write(x + "\n")
-                        f.close()
-                    elif response == 401:
-                        f = open("401.txt", "a")
-                        f.write(x + "\n")
-                        f.close()
-                    else:
-                        continue
+                    f = open(f"{response}.txt", "a")
+                    f.write(x + "\n")
+                    f.close()
                 elif answer == "N" or answer == "n":
                     print(x, response)
                 else:
                     continue
             except Exception as e:
+                f = open("error.txt", "a")
+                f.write(x + "\n")
+                f.close()
                 continue
 except Exception as e:
     pass
-    
